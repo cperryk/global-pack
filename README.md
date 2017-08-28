5c41c48e0a0cc79e24ad5770f40492cbab27929e
Chris Kirk <cperryk@gmail.com>
2017-08-28 11:00:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Global-pack
+
+Like [browser-pack](https://github.com/substack/browser-pack), but mounts modules to `window.modules` (or a specified scope). Allows splitting up a browser-pack bundle (e.g. a Browserify bundle) into separate module files that can be embedded onto a page via `<script>` tags.
---
+++ index.js
@@ -0,0 +1,31 @@
+const through = require('through2'),
+  input = require('./example/input'),
+  fs = require('fs'),
+  util = require('./util');
+
+/**
+ * Returns a through stream that takes in module-deps objects
+ * and emits global pack strings.
+ * @param {object} [opts]
+ * @param {string} [opts.scope='modules'] Property of window to contain modules
+ * @returns {Stream}
+ */
+function globalPack({scope = 'window.modules'} = {}) {
+  const prelude = util.getPrelude(scope),
+    postlude = util.getPostlude(scope);
+  let first = true;
+
+  return through.obj(function (dep, enc, cb) {
+    if (first) {
+      this.push(prelude);
+      first = false;
+    }
+    this.push(util.getModuleString(dep, scope));
+    cb();
+  }, function (cb) {
+    this.push(postlude);
+    cb();
+  });
+};
+
+module.exports = globalPack;
